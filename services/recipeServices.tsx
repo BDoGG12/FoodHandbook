ceed0d45ddce631b8a4d9dcd73223960e269e962
Ben Do <bdo.appworkshop@gmail.com>
2024-12-18 10:43:22 -0600

    Implemented fetchRecipes method.

---
+++ services/recipeServices.tsx
@@ -0,0 +1,15 @@
+
+export const fetchRecipes = async () => {
+    try {
+        const response = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+        if (!response.ok) {
+            throw new Error('Failed to fetch recipes')
+        }
+        const data = await response.json()
+        return data.results
+
+    } catch (e) {
+        console.log(e)
+        throw e
+    }
+}
