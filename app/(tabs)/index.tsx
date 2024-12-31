0d86433e18244793b8d2169825c53686f082e292
Ben Do <bdo.appworkshop@gmail.com>
2024-12-30 20:48:53 -0600

    Replaced ParallaxScrollView with SafeAreaView, inserted FlatList to render categories of food.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,42 +1,68 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, FlatList, SafeAreaView } from "react-native";
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { useState, useEffect } from "react";
+import { fetchCategories } from "@/services/recipeServices";
+import Category from "@/components/category/Category";
 
 export default function HomeScreen() {
+  const [categories, setCategories] = useState();
+
+  useEffect(() => {
+    const getCategories = async () => {
+      try {
+        const data = await fetchCategories();
+        setCategories(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getCategories();
+  }, []);
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
+    <SafeAreaView>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Featured Recipes</ThemedText>
       </ThemedView>
-    </ParallaxScrollView>
+      <FlatList
+        data={categories}
+        renderItem={({ item }) => (
+          <Category
+            categoryName={item.strCategory}
+            imageSource={item.strCategoryThumb}
+          />
+        )}
+        style={styles.listContainer}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
+    margin: 'auto',
+    color: 'black',
   },
   stepContainer: {
     gap: 8,
     marginBottom: 8,
+  },
+  listContainer: {
+    marginBottom: 15
   },
   reactLogo: {
     height: 178,
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
   },
 });
