8f91929781786473fadd137fcd3d3d0a36451780
Ben Do <bdo.appworkshop@gmail.com>
2025-01-03 15:27:37 -0600

    Implemented api service for fetching recipes by category and first letter.

--- services/recipeServices.tsx
+++ services/recipeServices.tsx
@@ -6,10 +6,39 @@ export const fetchCategories = async () => {
             throw new Error('Failed to fetch recipes')
         }
         const data = await response.json()
-        return data.results
+        return data.categories
 
     } catch (e) {
         console.log(e)
         throw e
+    }
+}
+
+export const fetchRecipeByCategory = async (category: string) => {
+
+    try {
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`)
+        if (!response.ok) {
+            throw new Error('Failed to fetch recipes')
+        }
+        const data = await response.json()
+        return data.results
+    } catch (error) {
+        console.log(error)
+        throw error
+    }
+}
+
+export const fetchMealsByFirstLetter = async (letter: string) => {
+    try {
+        const response = await fetch(`www.themealdb.com/api/json/v1/1/search.php?f=${letter}`)
+        if (!response.ok) {
+            throw new Error('Failed to fetch recipes')
+        }
+        const data = await response.json()
+        return data.results
+    } catch (error) {
+        console.log(error)
+        throw error
     }
 }
